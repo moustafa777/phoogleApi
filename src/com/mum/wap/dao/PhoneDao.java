62681978b83bbfa69b945d07e204ccd4db29ff92
Moustafa <moustafa.sayed66@gmail.com>
2018-07-17 12:11:21 -0500

    phone dao

--- src/com/mum/wap/dao/PhoneDao.java
+++ src/com/mum/wap/dao/PhoneDao.java
@@ -22,36 +22,7 @@ public class PhoneDao {
 			
 		}
 		 return phones;
-		
-/*		URL url = new URL("https://fonoapi.freshpixl.com/v1/getlatest");
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setDoOutput(true);
-		conn.setRequestMethod("POST");
-		conn.setRequestProperty("brand", brand);
-		conn.setRequestProperty("token", "64abed472ad1fb124ef8795557952077f51c538873c2a440");
-		conn.setRequestProperty("Content-Type", "application/json");
-
-		//String input = "{\"brand\":samsung}";
-
-		//OutputStream os = conn.getOutputStream();
-		//getClass().os.write(input.getBytes());
-		//os.flush();
-
-		if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
-			throw new RuntimeException("Failed : HTTP error code : "
-				+ conn.getResponseCode());
-		}
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				(conn.getInputStream())));
-
-		String output;
-		System.out.println("Output from Server .... \n");
-		while ((output = br.readLine()) != null) {
-			System.out.println(output);
-		}
-
-		conn.disconnect();*/
+	
 	}
 	
 }
